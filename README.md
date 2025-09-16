509a46672bb5cc4909921a75d4940f20a8bbc191
sophiawillingham <sophiwilli@gmail.com>
2025-09-16 11:43:14 -0500

    set up starter project

---
+++ README.md
@@ -0,0 +1,24 @@
+Sophia Willingham
+
+Identify the parts of the code that do each of these 3 things
+
+A ConstraintLayout with one TextView
+this is done in the activity_main.xml
+
+Defined string resources
+this is done in strings.xml
+
+A main activity with three helper methods
+that we will use to populate the ListView
+
+private void populateListView(int selection) {
+switch (selection) {
+case 1:
+useStringResource();
+break;
+case 2:
+usingDataArray();
+break;
+        }
+private void useStringResource() {
+private void usingDataArray() {
---
+++ app/src/main/java/Pet.java
@@ -0,0 +1,40 @@
+public class Pet {
+
+    String name;
+    String breed;
+    int age;
+
+    public Pet() {
+    }
+    public Pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
